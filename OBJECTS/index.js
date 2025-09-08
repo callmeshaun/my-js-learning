6883732eaffd4dfa0292294f1936aac0e6728f3e
shaunakk <callmeshaun@users.noreply.github.com>
2025-09-08 13:22:10 +0530

    Objects

---
+++ OBJECTS/index.js
@@ -0,0 +1,35 @@
+// objects = A collection of properties
+// Properties = A key-value pair
+// Object = { key: 'value' ,function()}
+
+// const person = {
+//   name: 'John',
+//   age: 30,  
+//   isEmployed: true,
+// }
+
+// console.log(person.name) // Dot notation
+// console.log(person['age']) // Bracket notation;
+// console.log(person.isEmployed)
+
+const person= {
+  name: 'Shaunak',
+  age: 20,
+  isEmployed: false,
+  sayHello: ()=> {
+    console.log('Hello! I am shaunak')},
+}
+
+console.log(person.name) // Dot notation
+console.log(person['age']) // Bracket notation
+console.log(person.isEmployed);
+person.sayHello() // Calling the method
+
+
+
+
+
+
+
+
+
