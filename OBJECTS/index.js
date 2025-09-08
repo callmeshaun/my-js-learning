b63de7e0c556e685e10d5cc9e74eed5ff4ce75d5
shaunakk <callmeshaun@users.noreply.github.com>
2025-09-08 18:19:13 +0530

    Objects

--- OBJECTS/index.js
+++ OBJECTS/index.js
@@ -12,22 +12,37 @@
 // console.log(person['age']) // Bracket notation;
 // console.log(person.isEmployed)
 
-const person= {
-  name: 'Shaunak',
-  age: 20,
-  isEmployed: false,
-  sayHello: ()=> {
-    console.log('Hello! I am shaunak')},
-}
+// const person= {
+//   name: 'Shaunak',
+//   age: 20,
+//   isEmployed: false,
+//   sayHello: ()=> {
+//     console.log('Hello! I am shaunak')},
+// }
 
-console.log(person.name) // Dot notation
-console.log(person['age']) // Bracket notation
-console.log(person.isEmployed);
-person.sayHello() // Calling the method
+// console.log(person.name) // Dot notation
+// console.log(person['age']) // Bracket notation
+// console.log(person.isEmployed);
+// person.sayHello() // Calling the method
+
+const person = [
+  { name: 'Shaunak',
+    age: 20,
+    isEmployed: false,},
+    { name: 'Swaraj',
+    age: 20,
+    isEmployed: true,},
+  { name: 'John',
+    age: 30,
+    isEmployed: true,},
+]
+
+const user = Object.groupBy(person, (users)=> users.age)
 
 
 
 
+console.log(user);
 
 
 
