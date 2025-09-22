29a124fdeec189257082df9677d2a31e6a64b81a
shaunakk <callmeshaun@users.noreply.github.com>
2025-09-22 18:40:47 +0530

    dates

---
+++ DATES/index.js
@@ -0,0 +1,28 @@
+// Date-objects = Objects that contain values that represent dates and
+//                times These date objects can be changed and formated
+
+// const date = new Date();
+// const year = date.getFullYear();
+// const month = date.getMonth()
+// const day = date.getDate();
+// const hours = date.getHours();
+// const minutes = date.getMinutes();
+// const seconds = date.getSeconds();
+
+// console.log(year);;
+// console.log(month);
+// console.log(date);
+// console.log(day);
+// console.log(hours);
+// console.log(minutes);
+// console.log(seconds);
+
+const date = new Date()
+date.setFullYear(2024)
+date.setMonth(11)
+date.setDate(24)
+date.setHours(0)
+date.setMinutes(0)
+date.setSeconds(0)
+
+console.log(date);
