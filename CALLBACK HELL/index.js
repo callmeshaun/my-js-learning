732e561e0ccc08806d22df60c0b24456c48165bc
shaunakk <callmeshaun@users.noreply.github.com>
2026-01-14 18:53:11 +0530

    CALLBACK HELL

--- CALLBACK HELL/index.js
+++ CALLBACK HELL/index.js
@@ -4,22 +4,39 @@
 //                 Old pattern to handle asynchronous functions.
 //                  Use Promises + async/await to avoid Callback Hell
 
-function task1(){
-  console.log("Task1 complete")
+function task1(callback){
+  setTimeout(()=>{
+    console.log("Task1 complete")
+    callback()
+  },2000)
 }
-function task2(){
-  console.log("Task2 complete")
+function task2(callback){
+  setTimeout(()=>{
+    console.log("Task2 complete")
+    callback()
+  },1000)
 }
-function task3(){
-  console.log("Task3 complete")
+function task3(callback){
+  setTimeout(()=>{
+    console.log("Task3 complete")
+    callback()
+  },3000)
 }
-function task4(){
-  console.log("Task4 complete")
+function task4(callback){
+  setTimeout(()=>{
+    console.log("Task4 complete")
+    callback()
+  },1500)
 }
 
-task1()
-task2()
-task3()
-task4()
-console.log("All tasks complete")
+task1(()=>{
+  task2(()=>{
+    task3(()=>{
+      task4(()=>{
+        console.log("All tasks complete")
+      })
+    })
+  })
+})
+
 
