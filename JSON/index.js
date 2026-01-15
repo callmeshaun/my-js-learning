1217b6c5c93ef0b10e7086a0231ecd2553eb2b46
shaunakk <callmeshaun@users.noreply.github.com>
2026-01-15 11:41:11 +0530

    JSON

---
+++ JSON/index.js
@@ -0,0 +1,58 @@
+//JSON - JavaScript Object Notation data interchange format
+//        used for data exchange between a server and a web application
+//        Json files {key:value} OR [value1 , value2 , value3]
+
+//        JSON.stringify() - converts a JavaScript object into a JSON string
+//        JSON.parse() - converts a JSON string into a JavaScript object
+
+const jsonNames = `["John", "Jane", "Jack", "Jill"]`
+const jsonPerson = `{
+  "name": "John",
+  "age": 30,
+  "isEmployed": true,
+  "hobbies": ["reading", "traveling", "sports"]
+}`;
+
+const jsonPeople = `[{"name": "John",
+  "age": 30,
+  "isEmployed": true
+  
+},
+
+{
+  "name": "Jane",
+  "age": 25,
+  "isEmployed": false
+},
+
+{
+  "name": "Jack",
+  "age": 35,
+  "isEmployed": true
+
+},
+
+{
+  "name": "Jill",
+  "age": 28,
+  "isEmployed": false
+
+}]`
+
+// const jsonString = JSON.stringify(names)
+// console.log(jsonString)
+
+// const jsonPerson = JSON.stringify(person)
+// console.log(jsonPerson)
+
+// const jsonPeople = JSON.stringify(people)
+// console.log(jsonPeople)
+
+// const parseData = JSON.parse(jsonNames)
+// console.log(parseData)
+
+// const parseData = JSON.parse(jsonPerson)
+// console.log(parseData);
+
+const parseData = JSON.parse(jsonPeople)
+console.log(parseData)
