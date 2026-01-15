221064dada3f3da38326e79cd1545dd10d73cd6c
shaunakk <callmeshaun@users.noreply.github.com>
2026-01-15 11:46:56 +0530

    JSON

--- JSON/index.js
+++ JSON/index.js
@@ -1,58 +1,65 @@
-//JSON - JavaScript Object Notation data interchange format
-//        used for data exchange between a server and a web application
-//        Json files {key:value} OR [value1 , value2 , value3]
+// //JSON - JavaScript Object Notation data interchange format
+// //        used for data exchange between a server and a web application
+// //        Json files {key:value} OR [value1 , value2 , value3]
 
-//        JSON.stringify() - converts a JavaScript object into a JSON string
-//        JSON.parse() - converts a JSON string into a JavaScript object
+// //        JSON.stringify() - converts a JavaScript object into a JSON string
+// //        JSON.parse() - converts a JSON string into a JavaScript object
 
-const jsonNames = `["John", "Jane", "Jack", "Jill"]`
-const jsonPerson = `{
-  "name": "John",
-  "age": 30,
-  "isEmployed": true,
-  "hobbies": ["reading", "traveling", "sports"]
-}`;
+// const jsonNames = `["John", "Jane", "Jack", "Jill"]`
+// const jsonPerson = `{
+//   "name": "John",
+//   "age": 30,
+//   "isEmployed": true,
+//   "hobbies": ["reading", "traveling", "sports"]
+// }`;
 
-const jsonPeople = `[{"name": "John",
-  "age": 30,
-  "isEmployed": true
+// const jsonPeople = `[{"name": "John",
+//   "age": 30,
+//   "isEmployed": true
   
-},
+// },
 
-{
-  "name": "Jane",
-  "age": 25,
-  "isEmployed": false
-},
+// {
+//   "name": "Jane",
+//   "age": 25,
+//   "isEmployed": false
+// },
 
-{
-  "name": "Jack",
-  "age": 35,
-  "isEmployed": true
+// {
+//   "name": "Jack",
+//   "age": 35,
+//   "isEmployed": true
 
-},
+// },
 
-{
-  "name": "Jill",
-  "age": 28,
-  "isEmployed": false
+// {
+//   "name": "Jill",
+//   "age": 28,
+//   "isEmployed": false
 
-}]`
+// }]`
 
-// const jsonString = JSON.stringify(names)
-// console.log(jsonString)
+// // const jsonString = JSON.stringify(names)
+// // console.log(jsonString)
 
-// const jsonPerson = JSON.stringify(person)
-// console.log(jsonPerson)
+// // const jsonPerson = JSON.stringify(person)
+// // console.log(jsonPerson)
 
-// const jsonPeople = JSON.stringify(people)
-// console.log(jsonPeople)
+// // const jsonPeople = JSON.stringify(people)
+// // console.log(jsonPeople)
 
-// const parseData = JSON.parse(jsonNames)
+// // const parseData = JSON.parse(jsonNames)
+// // console.log(parseData)
+
+// // const parseData = JSON.parse(jsonPerson)
+// // console.log(parseData);
+
+// const parseData = JSON.parse(jsonPeople)
 // console.log(parseData)
 
-// const parseData = JSON.parse(jsonPerson)
-// console.log(parseData);
 
-const parseData = JSON.parse(jsonPeople)
-console.log(parseData)
+//promise
+fetch("people.json")
+.then(response => response.json())
+.then(values => values.forEach(value => console.log(value)))
+.catch(error => console.log(error))
